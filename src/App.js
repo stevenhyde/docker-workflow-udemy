0ad9fc67c2fd88a7d652b83c764efe4b6a26c3f3
Steven Hyde <preklady@gmail.com>
2019-01-27 17:32:14 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload.	   
+          </p>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.	   
           </p>
           <a
             className="App-link"
@@ -17,7 +20,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React and test it in docker-compose!
           </a>
         </header>
       </div>
